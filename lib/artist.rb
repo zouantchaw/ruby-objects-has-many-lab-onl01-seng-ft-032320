bc595f3b1bd24ede0b53ce41c8fdc3cdcedd4456
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-07 18:06:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,24 +3,24 @@ class Artist
   
   def initialize(name)
     @name = name 
-  end
-  
-  def add_song(song)
-    song.artist = self
-  end
-  
-  def add_song_by_name(name)
-    song = Song.new(name)
-    add_song(song)
-  end
-  
-  def self.song_count 
-    Song.all.count
-  end
+  end 
   
   def songs 
     Song.all.select{|song|
-      song.artist == self
+      song.artist == self 
     }
-  end
-end
+  end 
+  
+  def add_song(song)
+    song.artist = self 
+  end 
+  
+  def add_song_by_name(song)
+    song = Song.new(name)
+    add_song(song)
+  end 
+  
+  def self.song_count 
+    Song.all.count
+  end 
+end 
