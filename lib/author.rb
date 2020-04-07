e516ab450c7b5c343661dfdcc6de98713981691e
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-07 18:19:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,9 +5,22 @@ class Author
     @name = name 
   end 
   
+  def add_post(post)
+    post.author = self
+  end 
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(title)
+  end 
+  
   def posts 
     Post.all.select{|post|
       post.author == self
     }
+  end 
+  
+  def self.post_count 
+    Post.all.count
   end 
 end 
